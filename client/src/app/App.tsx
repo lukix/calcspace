bd5cf4d41ddecbd39c23d3f6ac37c6fb7e41ec7c
lukix <lukix.95@gmail.com>
2019-12-22 15:49:46 +0100

    Add new card icon

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaPlus } from 'react-icons/fa';
 import NoteCard from '../noteCard/NoteCard';
 import styles from './App.module.scss';
 
@@ -7,7 +8,12 @@ interface AppProps {}
 const App: React.FC<AppProps> = () => {
   return (
     <div>
-      <div className={styles.headerBar}>Math Notes</div>
+      <div className={styles.headerBar}>
+        <div className={styles.headerTitle}>Math Notes</div>
+        <div className={styles.icons}>
+          <FaPlus className={styles.addNewCardIcon} title="Add new card" />
+        </div>
+      </div>
       <div className={styles.contentContainer}>
         <NoteCard initialList={[{ value: '' }]} />
       </div>
