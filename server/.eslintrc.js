c284080c341da22c40afd47b6f433cc79c32a2ff
Łukasz Jenczmyk <lukix.95@gmail.com>
2020-01-04 22:11:08 +0100

    Fix eslint config for server app

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,15 +2,18 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:jest/recommended',
-    // 'airbnb-base',
-    // 'plugin:@typescript-eslint/eslint-recommended',
-    // 'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  plugins: [
-    'jest',
-    // '@typescript-eslint',
-  ],
-  // parser: '@typescript-eslint/parser',
+  plugins: ['jest', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
   env: {
     es6: true,
     node: true,
