e6039ae8571818a5c9eb69458021f0f6d63d9c0c
Łukasz Jenczmyk <lukix.95@gmail.com>
2020-04-19 22:49:36 +0200

    Stop renewing cookie in each request

--- server/src/auth/authorizationMiddleware.ts
+++ server/src/auth/authorizationMiddleware.ts
@@ -1,6 +1,10 @@
 import jwt from 'jsonwebtoken';
 import getNewJwtTokenCookie from './getNewJwtTokenCookie';
-import { JWT_SECRET_KEY, JWT_TOKEN_COOKIE_NAME } from '../config';
+import {
+  JWT_SECRET_KEY,
+  JWT_TOKEN_COOKIE_NAME,
+  TOKEN_RENEW_AFTER_MS,
+} from '../config';
 
 const authorizationMiddleware = async (req, res, next) => {
   const jwtToken = req.cookies[JWT_TOKEN_COOKIE_NAME];
@@ -8,13 +12,22 @@ const authorizationMiddleware = async (req, res, next) => {
     return res.sendStatus(403);
   }
   try {
-    const { userId, username } = jwt.verify(jwtToken, JWT_SECRET_KEY);
-    const jwtTokenCookie = getNewJwtTokenCookie(userId, username);
-    res.cookie(
-      jwtTokenCookie.name,
-      jwtTokenCookie.value,
-      jwtTokenCookie.options
+    const { userId, username, iat: issuedAtTime } = jwt.verify(
+      jwtToken,
+      JWT_SECRET_KEY
     );
+
+    const shouldRenewToken =
+      new Date().getTime() - issuedAtTime * 1e3 > TOKEN_RENEW_AFTER_MS;
+    if (shouldRenewToken) {
+      const jwtTokenCookie = getNewJwtTokenCookie(userId, username);
+      res.cookie(
+        jwtTokenCookie.name,
+        jwtTokenCookie.value,
+        jwtTokenCookie.options
+      );
+    }
+
     req.user = { userId, username };
   } catch (err) {
     return res.sendStatus(403);
--- server/src/config.ts
+++ server/src/config.ts
@@ -8,7 +8,10 @@ export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'secret123';
 export const TOKEN_EXPIRATION_DURATION_MS = process.env
   .TOKEN_EXPIRATION_DURATION_MS
   ? Number(process.env.TOKEN_EXPIRATION_DURATION_MS)
-  : 30 * 60 * 1000;
+  : 60 * 60 * 1000;
+export const TOKEN_RENEW_AFTER_MS = process.env.TOKEN_RENEW_AFTER_MS
+  ? Number(process.env.TOKEN_RENEW_AFTER_MS)
+  : 5 * 60 * 1000;
 export const SALT_ROUNDS = 14;
 export const JWT_TOKEN_COOKIE_NAME = 'jwtToken';
 export const SIGN_OUT_URL = `${CLIENT_URL}`;
