ef2b4731509390f98c59c5bf3074737b5f48e54e
Łukasz Jenczmyk <lukix.95@gmail.com>
2020-05-05 20:43:15 +0200

    Improve unitToString function

--- client/src/filePage/codeEditor/mathParser/unitToString.ts
+++ client/src/filePage/codeEditor/mathParser/unitToString.ts
@@ -1,8 +1,22 @@
+const sortBySymbol = (unitObjects) =>
+  [...unitObjects].sort((unitObjA, unitObjB) => unitObjA.unit.localeCompare(unitObjB.unit));
+
 const unitToString = (unitObjects: Array<{ unit: string; power: number }>): string => {
-  return [...unitObjects]
-    .sort((unitObjA, unitObjB) => unitObjA.unit.localeCompare(unitObjB.unit))
-    .map(({ unit, power }) => (power === 1 ? unit : `${unit}^${power}`))
-    .join('*');
+  const unitsWithPositivePowers = sortBySymbol(unitObjects.filter(({ power }) => power > 0));
+  const unitsWithNegativePowers = sortBySymbol(unitObjects.filter(({ power }) => power < 0));
+
+  if (unitsWithPositivePowers.length > 0) {
+    const positivePowersString = unitsWithPositivePowers
+      .map(({ unit, power }) => (power === 1 ? unit : `${unit}^${power}`))
+      .join('*');
+    const negativePowersString = unitsWithNegativePowers
+      .map(({ unit, power }) => (Math.abs(power) === 1 ? unit : `${unit}^${Math.abs(power)}`))
+      .join('/');
+
+    return `${positivePowersString}${negativePowersString && `/${negativePowersString}`}`;
+  }
+
+  return unitsWithNegativePowers.map(({ unit, power }) => `${unit}^${power}`).join('*');
 };
 
 export default unitToString;
