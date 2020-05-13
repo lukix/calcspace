d9ce8d0e773788f6160f44758abff843c66d50d7
Łukasz Jenczmyk <lukix.95@gmail.com>
2020-05-13 22:15:21 +0200

    Add form validation

--- client/src/app/UserProfileModal.tsx
+++ client/src/app/UserProfileModal.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { Modal, ModalFormField, SubmitButton } from '../shared/modal';
+
+const validationSchema = yup.object().shape({
+  currentPassword: yup.string().label('Current Password').required(),
+  newPassword: yup.string().label('New Password').min(6).max(72).required(),
+  repeatNewPassword: yup
+    .mixed()
+    .label('Repeat New Password')
+    .oneOf([yup.ref('newPassword')], "Passwords don't match")
+    .required(),
+});
 
 interface UserProfileModalProps {
   visible: boolean;
@@ -14,7 +25,7 @@ const UserProfileModal: React.FC<UserProfileModalProps> = ({ visible, onHide }) 
       newPassword: '',
       repeatNewPassword: '',
     },
-    // validationSchema,
+    validationSchema,
     onSubmit: async ({ currentPassword, newPassword, repeatNewPassword }, formikProps) => {
       console.log({ currentPassword, newPassword, repeatNewPassword });
     },
@@ -23,20 +34,27 @@ const UserProfileModal: React.FC<UserProfileModalProps> = ({ visible, onHide }) 
   return (
     <Modal visible={visible} onHide={onHide}>
       <h2>Change Password</h2>
-      <ModalFormField
-        type="text"
-        name="currentPassword"
-        label="Current Password"
-        formikProps={formik}
-      />
-      <ModalFormField type="text" name="newPassword" label="New Password" formikProps={formik} />
-      <ModalFormField
-        type="text"
-        name="repeatNewPassword"
-        label="Repeat New Password"
-        formikProps={formik}
-      />
-      <SubmitButton value="Change Password" disabled={false} />
+      <form onSubmit={formik.handleSubmit}>
+        <ModalFormField
+          type="password"
+          name="currentPassword"
+          label="Current Password"
+          formikProps={formik}
+        />
+        <ModalFormField
+          type="password"
+          name="newPassword"
+          label="New Password"
+          formikProps={formik}
+        />
+        <ModalFormField
+          type="password"
+          name="repeatNewPassword"
+          label="Repeat New Password"
+          formikProps={formik}
+        />
+        <SubmitButton value="Change Password" disabled={false} />
+      </form>
     </Modal>
   );
 };
