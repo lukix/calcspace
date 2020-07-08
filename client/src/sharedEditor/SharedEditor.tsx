3730a433c6a2a67f4450c6bfb4e9871ee394c81a
Łukasz Jenczmyk <lukix.95@gmail.com>
2020-07-08 21:55:15 +0200

    Extract useSharedFileChangeListener hook from shared editor

--- client/src/sharedEditor/SharedEditor.tsx
+++ client/src/sharedEditor/SharedEditor.tsx
@@ -1,16 +1,11 @@
 import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react';
-import socketIO from 'socket.io-client';
-import * as diff from 'diff';
-
-import { SOCKETS_URL } from '../config';
 import CodeEditor from '../shared/codeEditor';
 
 import httpRequest from '../shared/httpRequest';
 import SyncService, { requestLimiterMethods } from '../shared/syncService';
 
 import { syncStatuses } from './constants';
-import findNewCursorPosition from './findNewCursorPosition';
-import mergeChanges from './mergeChanges';
+import useSharedFileChangeListener from './useSharedFileChangeListener';
 
 interface SharedEditorProps {
   id: string;
@@ -30,8 +25,22 @@ const SharedEditor: React.FC<SharedEditorProps> = ({
     commitId: initialFileCommit.commitId,
     code: initialFileCommit.code,
   });
-  const [socket, setSocket] = useState<any>(null);
+
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useSharedFileChangeListener({
+    socketSubscribePath: viewOnly ? 'subscribe-to-file/by-view-id' : 'subscribe-to-file/by-edit-id',
+    id,
+    commit,
+    code,
+    selectionStart: textareaRef.current?.selectionStart,
+    selectionEnd: textareaRef.current?.selectionEnd,
+    changeHandler: useCallback(({ data, newCode, newSelectionStart, newSelectionEnd }) => {
+      setCode(newCode);
+      setCommit({ commitId: data.commitId, code: data.code });
+      textareaRef.current?.setSelectionRange(newSelectionStart, newSelectionEnd);
+    }, []),
+  });
 
   const syncService = useMemo(() => {
     return SyncService({
@@ -52,47 +61,6 @@ const SharedEditor: React.FC<SharedEditorProps> = ({
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [code, viewOnly, syncService]);
-
-  const handleSocketChangeMessage = useCallback(
-    (data) => {
-      if (!textareaRef.current) {
-        return;
-      }
-
-      const newCode = viewOnly ? data.code : mergeChanges(commit.code, code, data.code);
-      const { selectionStart, selectionEnd } = textareaRef.current;
-      const diffResult = diff.diffChars(code, newCode);
-      const newSelectionStart = findNewCursorPosition(diffResult, selectionStart);
-      const newSelectionEnd = findNewCursorPosition(diffResult, selectionEnd);
-
-      setCode(newCode);
-      setCommit({ commitId: data.commitId, code: data.code });
-
-      textareaRef.current.setSelectionRange(newSelectionStart, newSelectionEnd);
-    },
-    [commit, code, viewOnly]
-  );
-
-  useEffect(() => {
-    if (!socket) {
-      return;
-    }
-    socket.on('change', handleSocketChangeMessage);
-    return () => socket.off('change');
-  }, [socket, handleSocketChangeMessage]);
-
-  useEffect(() => {
-    const newSocket = socketIO(SOCKETS_URL);
-    newSocket.emit(viewOnly ? 'subscribe-to-file/by-view-id' : 'subscribe-to-file/by-edit-id', {
-      id,
-    });
-    setSocket(newSocket);
-
-    return () => {
-      newSocket.disconnect();
-      setSocket(null);
-    };
-  }, [id, viewOnly]);
 
   return (
     <CodeEditor code={code} onChange={setCode} textareaRef={textareaRef} viewOnly={viewOnly} />
---
+++ client/src/sharedEditor/useSharedFileChangeListener.ts
@@ -0,0 +1,53 @@
+import { useState, useEffect, useCallback } from 'react';
+import socketIO from 'socket.io-client';
+import * as diff from 'diff';
+
+import { SOCKETS_URL } from '../config';
+
+import findNewCursorPosition from './findNewCursorPosition';
+import mergeChanges from './mergeChanges';
+
+const useSharedFileChangeListener = ({
+  socketSubscribePath,
+  id,
+  commit,
+  code,
+  selectionStart,
+  selectionEnd,
+  changeHandler,
+}) => {
+  const [socket, setSocket] = useState<any>(null);
+
+  const handleSocketChangeMessage = useCallback(
+    (data) => {
+      const newCode = mergeChanges(commit.code, code, data.code);
+      const diffResult = diff.diffChars(code, newCode);
+      const newSelectionStart = findNewCursorPosition(diffResult, selectionStart);
+      const newSelectionEnd = findNewCursorPosition(diffResult, selectionEnd);
+
+      changeHandler({ data, newCode, newSelectionStart, newSelectionEnd });
+    },
+    [changeHandler, commit, code, selectionStart, selectionEnd]
+  );
+
+  useEffect(() => {
+    const newSocket = socketIO(SOCKETS_URL);
+    newSocket.emit(socketSubscribePath, { id });
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+      setSocket(null);
+    };
+  }, [id, socketSubscribePath]);
+
+  useEffect(() => {
+    if (!socket) {
+      return;
+    }
+    socket.on('change', handleSocketChangeMessage);
+    return () => socket.off('change');
+  }, [socket, handleSocketChangeMessage]);
+};
+
+export default useSharedFileChangeListener;
